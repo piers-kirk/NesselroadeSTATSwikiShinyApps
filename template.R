1016b72f88f2c7a86583ca604a0006536e7155ee
Piers Kirk <pfk3jd@virginia.edu>
2021-08-14 16:35:47 -0400

    implemented server function
    Added an if else statement inside the server function to implement a basic graph for the Bernoulli distribution.

--- template.R
+++ template.R
@@ -1,5 +1,4 @@
-## R Shiny Template
-## 02.05.21
+## 08.14.21
 ## Piers Kirk, pfk3jd
 
 source("/Users/pierskirk/Documents/GitHub/NesselroadeSTATSwikiShinyApps/functions_to_create_hists.R")
@@ -8,18 +7,21 @@ library(shiny)
 ui <- fluidPage(
   titlePanel("The Central Limit Theorem"),
   selectInput(
-    "distribution",
-    "Distribution:",
-    c("Bernoulli" = "Bernoulli",
-      "Bimodal" = "Bimodal",
-      "Binomial" = "Binomial",
-      "Chi-Square" = "Chi-Square",
-      "F" = "F",
-      "Logistic" = "Logistic",
-      "Normal Distribution (Gaussian)" = "Normal Distribution (Gaussian)",
-      "Poisson" = "Poisson",
-      "Student t" = "Student t",
-      "Uniform" = "Uniform")), 
+    inputId = "distribution",
+    label = "Distribution:",
+    c(
+      "Bernoulli" = "bernoulli",
+      "Bimodal" = "bimodal",
+      "Binomial" = "binomial",
+      "Chi-Square" = "chi_square",
+      "F" = "f",
+      "Logistic" = "logistic",
+      "Normal Distribution (Gaussian)" = "gaussian",
+      "Poisson" = "poisson",
+      "Student t" = "student_t",
+      "Uniform" = "uniform"
+    )
+  ),
   sidebarPanel(
     sliderInput(
       inputId = "num_of_samples",
@@ -27,16 +29,18 @@ ui <- fluidPage(
       min = 1,
       max = 10000,
       value = 10,
-      step = 1),
+      step = 1
+    ),
     sliderInput(
       inputId = "sample_size",
       label = "Sample Size",
       min = 1,
       max = 10000,
       value = 10,
-      step = 1),
+      step = 1
+    ),
   ),
-  conditionalPanel(condition = "input.distribution=='Bernoulli'|input.distribution=='Binomial'", 
+  conditionalPanel(condition = "input.distribution=='bernoulli'",
                    sidebarPanel(
                      sliderInput(
                        inputId = "prob",
@@ -44,20 +48,30 @@ ui <- fluidPage(
                        min = 0,
                        max = 1,
                        value = 0.5,
-                       step = 0.01),
-                   ),),
+                       step = 0.01
+                     ),
+                   ), ),
   ## INPUT SOMETHING FOR BIMODAL
-  conditionalPanel(condition = "input.distribution=='Binomial'", 
+  conditionalPanel(condition = "input.distribution=='binomial'",
                    sidebarPanel(
+                     sliderInput(
+                       inputId = "prob",
+                       label = "Probability",
+                       min = 0,
+                       max = 1,
+                       value = 0.5,
+                       step = 0.01
+                     ),
                      sliderInput(
                        inputId = "size",
                        label = "Size",
                        min = 1,
                        max = 1000,
                        value = 1,
-                       step = 10),
-                   ),),
-  conditionalPanel(condition = "input.distribution=='Chi-Square'|input.distribution=='Student t'", 
+                       step = 10
+                     ),
+                   ), ),
+  conditionalPanel(condition = "input.distribution=='chi_square'|input.distribution=='student_t'",
                    sidebarPanel(
                      sliderInput(
                        inputId = "df",
@@ -65,9 +79,10 @@ ui <- fluidPage(
                        min = 1,
                        max = 100,
                        value = 1,
-                       step = 1),
-                   ),),
-  conditionalPanel(condition = "input.distribution=='F'", 
+                       step = 1
+                     ),
+                   ), ),
+  conditionalPanel(condition = "input.distribution=='f'",
                    sidebarPanel(
                      sliderInput(
                        inputId = "df_1",
@@ -75,33 +90,37 @@ ui <- fluidPage(
                        min = 1,
                        max = 100,
                        value = 1,
-                       step = 1),
+                       step = 1
+                     ),
                      sliderInput(
                        inputId = "df_2",
                        label = "Degrees of Freedom 2",
                        min = 1,
                        max = 100,
                        value = 1,
-                       step = 1),
-                   ),),
-  conditionalPanel(condition = "input.distribution=='Logistic'", 
+                       step = 1
+                     ),
+                   ), ),
+  conditionalPanel(condition = "input.distribution=='logistic'",
                    sidebarPanel(
                      sliderInput(
-                       inputId = "location_",
+                       inputId = "location",
                        label = "Location (Mean)",
                        min = 1,
                        max = 100,
                        value = 1,
-                       step = 0.5),
+                       step = 0.5
+                     ),
                      sliderInput(
-                       inputId = "scale_",
+                       inputId = "scale",
                        label = "Scale (Variance)",
                        min = 1,
                        max = 100,
                        value = 1,
-                       step = 0.5),
-                   ),),
-  conditionalPanel(condition = "input.distribution=='Normal Distribution (Gaussian)'", 
+                       step = 0.5
+                     ),
+                   ), ),
+  conditionalPanel(condition = "input.distribution=='gaussian'",
                    sidebarPanel(
                      sliderInput(
                        inputId = "mean",
@@ -109,16 +128,18 @@ ui <- fluidPage(
                        min = 1,
                        max = 100,
                        value = 1,
-                       step = 0.5),
+                       step = 0.5
+                     ),
                      sliderInput(
                        inputId = "sd",
                        label = "Standard Deviation",
                        min = 1,
                        max = 100,
                        value = 1,
-                       step = 0.5),
-                   ),),
-  conditionalPanel(condition = "input.distribution=='Poisson'", 
+                       step = 0.5
+                     ),
+                   ), ),
+  conditionalPanel(condition = "input.distribution=='poisson'",
                    sidebarPanel(
                      sliderInput(
                        inputId = "lambda",
@@ -126,38 +147,37 @@ ui <- fluidPage(
                        min = 1,
                        max = 100,
                        value = 1,
-                       step = 0.5),
-                   ),),
-  conditionalPanel(condition = "input.distribution=='Uniform'", 
-                   sidebarPanel(
-                     sliderInput(
-                       inputId = "Range",
-                       label = "Range",
-                       min = 1,
-                       max = 100,
-                       value = c(1,10),
-                       step = 1,
-                       dragRange = TRUE),
-                   ),),
-  # Main panel for displaying outputs ----
-  mainPanel(# *Output() functions
-    plotOutput(outputId = "hist"))
+                       step = 0.5
+                     ),
+                   ), ),
+  conditionalPanel(
+    condition = "input.distribution=='uniform'",
+    sidebarPanel(
+      sliderInput(
+        inputId = "range",
+        label = "Range",
+        min = 1,
+        max = 100,
+        value = c(1, 10),
+        step = 1,
+        dragRange = TRUE,
+      ),
+    ),
+  ),
+  mainPanel(plotOutput(outputId = "my_hist"))
 )
 
 server <- function(input, output) {
-  output$hist <- renderPlot({hist(30,10)
-    ##input_to_hist(distributions_list, input$distribution, num_of_samples, sample_size)
-  })
+  output$my_hist <- renderPlot({ 
+    if (input$distribution == 'bernoulli') {
+      bern_dist_vec <- rbinom(population_size, size = 1, prob=input$prob)
+      sample_means_bern <- sampling_distribution(bern_dist_vec, input$num_of_samples, input$sample_size) 
+      hist(bern_dist_vec) 
+    } else {
+      hist(20, 20)
+    }
+    print("END")
+    })
 }
 
 shinyApp(ui = ui, server = server)
-
-################################################################
-## REFERENCES
-##
-## How to Start Shiny tutorial
-## Garrett Grolemund 
-## 02.05.21
-## https://shiny.rstudio.com/tutorial/#written-tutorials 
-##
-################################################################
